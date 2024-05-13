65126e9b4896847da292cbfe31de5a7c89d234ba
junhyeog <h-kun@naver.com>
2024-05-14 06:14:50 +0900

    Update:  펠린드롬_1213 문제

--- 팰린드롬/backjoon_1213.cpp
+++ 팰린드롬/backjoon_1213.cpp
@@ -12,13 +12,26 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> s;
-    //ex ) AAABB
+    //cin >> s;
+    string s = "ABBCCDD";
+
     // 카운팅 배열
+    /**
+     문자열 's'를 순회하면서 출현 빈도를 계산한다.
+     만약 'a' 가 'A' 라면 cnt[0] 에 접근을 하게 된다. cnt[0]++; 을 해서 카운트를 한다.
+     'a'가 'B' 라면 cnt[1] 에 접근한다. 아스키코드로 66-54 가 되기 때문이다.
+    **/
     for(char a : s) cnt[a - 'A']++;  // 문자의 출현 빈도를 계산한다.
+
+ //test
+//    for(int i = 0; i < s.size(); i++){
+//         cout << cnt[i] << "\n";
+//    }
+
     // Z -> A  까지 진행하는데 , 홀수이면
     for(int i = 'Z'; i >= 'A'; i--){
         if(cnt[i - 'A']){
+
             // 홀수
             if(cnt[i - 'A'] & 1){
                 mid = char(i); flag++;
