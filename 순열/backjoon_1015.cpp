752c6013e553064ff8e406c1ecec6fabdfd008a5
junhyeog <h-kun@naver.com>
2024-05-27 10:00:49 +0900

    Update:

--- 순열/backjoon_1015.cpp
+++ 순열/backjoon_1015.cpp
@@ -13,17 +13,19 @@ int main() {
         cin >> arr[i];
     }
 
-    vector<pair<int, int>> arr_index;
+    vector<pair<int, int>> arr_index;   //  [4, 2, 9]
     for (int i = 0; i < N; i++) {
-        arr_index.push_back({arr[i], i});
+        arr_index.push_back({arr[i], i});  //[(4, 0), (2, 1), (9, 2)]
     }
 
-    sort(arr_index.begin(), arr_index.end());
+    sort(arr_index.begin(), arr_index.end());  //[(2, 1), (4, 0), (9, 2)]
+
 
     vector<int> p(N);
     for (int i = 0; i < N; i++) {
-        p[arr_index[i].second] = i;
-    }
+        p[arr_index[i].second] = i;   // p[1] = 0 , p[0] = 1 , p[2] = 2
+    }  // [1, 0, 2]
+
 
     for (auto a : p) {
         cout << a << ' ';
